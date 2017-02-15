b53ad741be81ca15dc71f0cb78802a4712448625
Edgar Cirilo <edcirilo_9216@live.com.mx>
2017-02-15 10:57:29 -0600

    New project from archetype org.codehaus.gmaven.archetypes:gmaven-archetype-basic

---
+++ src/test/groovy/com/edcg/HelperTest.groovy
@@ -0,0 +1,19 @@
+//
+// Generated from archetype; please customize.
+//
+
+package com.edcg
+
+import com.edcg.Helper
+import com.edcg.Example
+
+/**
+ * Tests for the {@link Helper} class.
+ */
+class HelperTest
+    extends GroovyTestCase
+{
+    void testHelp() {
+        new Helper().help(new Example())
+    }
+}
